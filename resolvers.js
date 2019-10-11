905fcf4870d434d4013e26a3eefaad3fec042ef1
Justin Graham <support@pcrobinhood.com>
2019-10-11 13:41:20 -0500

    Added more note functions

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,23 @@
-import Note from './models/note'
+import Note from "./models/note"
 
 export const resolvers = {
-    Query : {
-       async allNotes(){
-           return await Note.find()
-       }
+  Query: {
+    async getNote(root, {_id}) {
+      return await Note.findById(_id)
+    },
+    async allNotes() {
+      return await Note.findAll()
     }
+  },
+  Mutation: {
+      async createContext(root, {input}) {
+          return await Note.create(input)
+      },
+      async updateNote(root, {_id, input}) {
+          return await Note.findAndUpdate({_id}, input, {new: true})
+      },
+      async deleteNote(root, {_id }){
+          return await Note.findOneAndDelete({_id})
+      }
+  }
 }
--- schema.js
+++ schema.js
@@ -8,10 +8,28 @@ const typeDefs = `
   date: Date,
   content: String!
  }
+
 scalar Date
+
 type Query {
   allNotes: [Note]
  }
+
+ input NoteInput {
+    title: String!
+    content: String!
+}
+
+input NoteUpdateInput {
+ title: String
+ content: String
+}
+
+type Mutation {
+ createNote(input: NoteInput) : Note
+ updateNote(_id: ID!, input: NoteUpdateInput): Note
+ deleteNote(_id: ID!) : Note
+}
 `
 const schema = makeExecutableSchema({
   typeDefs,
